455ac478dbdf995315fa486095e2a86949abcc8b
GulshanSS <sahugulshan686@gmail.com>
2023-07-30 20:17:28 +0530

    Add: Required data for Article Page

---
+++ src/app/article/[articleId]/page.tsx
@@ -0,0 +1,34 @@
+import ArticleFeed from "@/app/components/Article/ArticleFeed";
+import LatestFeed from "@/app/components/Latest/LatestFeed";
+import PromotionaryArticleFeed from "@/app/components/PromotionaryArticle/PromotionaryArticleFeed";
+import { getArticleById, getLatestArticles } from "@/app/lib/article";
+import { getAllCategories } from "@/app/lib/category";
+import { getAllPromotionaryArticlesAsBanners } from "@/app/lib/promotionaryArticles";
+import React from "react";
+
+type Params = {
+  params: {
+    articleId: string;
+  };
+};
+
+export default async function ArticlePage({ params: { articleId } }: Params) {
+  const { article } = await getArticleById(articleId);
+  const { articles: latestArticles } = await getLatestArticles();
+  const { promotionaryArticles: promotionaryArticlesAsBanner } =
+    await getAllPromotionaryArticlesAsBanners();
+
+  return (
+    <main className="max-w-screen-xl flex flex-wrap mx-auto text-3xl mt-32 font-bold">
+      <div className="md:w-2/3">
+        <ArticleFeed article={article} />
+      </div>
+      <div className="md:w-1/3">
+        <LatestFeed articles={latestArticles} />
+        <PromotionaryArticleFeed
+          promotionaryArticles={promotionaryArticlesAsBanner}
+        />
+      </div>
+    </main>
+  );
+}
