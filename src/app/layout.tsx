b84bc5282ebfc334ec6c2dc7d01afeeebe3ca679
GulshanSS <sahugulshan686@gmail.com>
2023-07-30 20:16:49 +0530

    Add: Send categories to nav

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,25 +1,34 @@
 import "./globals.css";
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Poppins } from "next/font/google";
 import Navbar from "./components/Navigation/Navbar";
+import SubNav from "./components/Navigation/SubNav";
+import Footer from "./components/Footer/Footer";
+import { getAllCategories } from "./lib/category";
 
-const inter = Inter({ subsets: ["latin"] });
+const poppins = Poppins({ weight: "500", subsets: ["latin-ext"] });
 
 export const metadata: Metadata = {
   title: "NagpurNews",
   description: "Daily news lookup",
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const categoryData = getAllCategories();
+
+  const { categories } = await categoryData;
+
   return (
     <html lang="en">
-      <body className={inter.className}>
-        <Navbar />
+      <body className={poppins.className}>
+        <Navbar categories={categories} />
+        <SubNav />
         {children}
+        <Footer />
       </body>
     </html>
   );
