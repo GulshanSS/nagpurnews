b74d7f8ff0cd46ce49f32a201d5f74a1fbfdeff6
GulshanSS <sahugulshan686@gmail.com>
2023-07-30 20:20:47 +0530

    Add: Promotionary Article Feed Component

---
+++ src/app/components/PromotionaryArticle/PromotionaryArticleFeed.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { PromotionaryArticle } from "../../../../types";
+import PromotionaryArticleCard from "../shared/PromotionaryArticleCard";
+
+type Props = {
+  promotionaryArticles: PromotionaryArticle[];
+};
+
+export default function PromotionaryArticleFeed({
+  promotionaryArticles,
+}: Props) {
+  return (
+    <>
+      <div className="mx-2 p-2">
+        <div className="my-4 flex flex-col gap-2">
+          {promotionaryArticles.map((promotionaryArticle) => (
+            <PromotionaryArticleCard
+              key={promotionaryArticle.id}
+              promotionaryArticle={promotionaryArticle}
+            />
+          ))}
+        </div>
+      </div>
+    </>
+  );
+}
