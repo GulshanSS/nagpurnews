d2576656bc6442a35a6e55efd0b8500dd17bc784
GulshanSS <sahugulshan686@gmail.com>
2023-07-30 20:19:05 +0530

    Add: Explore Category Component

---
+++ src/app/components/Category/ExploreCategory.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { Category } from "../../../../types";
+import Link from "next/link";
+
+type Props = {
+  categories: Category[];
+};
+
+export default function ExploreCategory({ categories }: Props) {
+  return (
+    <div className="mx-2 p-4 flex flex-col rounded-md bg-primary-900">
+      <span className="mb-6 font-semibold text-2xl text-primary-100 uppercase">
+        Explore Other Categories
+      </span>
+      <div className="flex flex-wrap gap-2 text-primary-100">
+        {categories.map((category) => (
+          <Link href={`/category/${category.id}`}>
+            <div
+              key={category.id}
+              className="uppercase text-[12px] px-2.5 hover:bg-primary-100 hover:text-primary-800 border-2 border-primary-100 rounded-md cursor-pointer"
+            >
+              {category.name}
+            </div>
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
+}
