5bf2a7d2c054850a8f89a4ce4e1cd156cad44e2e
GulshanSS <sahugulshan686@gmail.com>
2023-07-30 20:20:00 +0530

    Add: Social link to navbar

--- src/app/components/Navigation/Navbar.tsx
+++ src/app/components/Navigation/Navbar.tsx
@@ -4,11 +4,18 @@ import Image from "next/image";
 import logo from "@/app/assets/logo.jpg";
 import { FiChevronsRight, FiChevronsLeft, FiSearch } from "react-icons/fi";
 import { useState } from "react";
-import NavPill from "./NavPill";
+import SocialLinkForPromotionaryArticle from "../shared/SocialLinkForPromotionaryArticle";
+import { BsFacebook, BsInstagram, BsTwitter, BsWhatsapp } from "react-icons/bs";
+import { BiSolidCategory } from "react-icons/bi";
+import { Category } from "../../../../types";
+import Link from "next/link";
 
-export default function Navbar() {
+type Props = {
+  categories: Category[];
+};
+
+export default function Navbar({ categories }: Props) {
   const [open, setOpen] = useState(true);
-
   return (
     <>
       <nav className="top-0 fixed w-full bg-primary-50 border-b border-primary-600 z-50">
@@ -16,14 +23,14 @@ export default function Navbar() {
           <a href="/" className="flex items-center">
             <Image
               src={logo}
-              className="w-8 mr-3 rounded-full"
+              className="w-8 mr-3 rounded-full ring-2 ring-primary-600"
               alt="Nagpur News Logo"
             />
-            <span className="self-center text-2xl font-semibold whitespace-nowrap">
-              Nagpur News
+            <span className="self-center text-2xl text-primary-800 font-semibold whitespace-nowrap">
+              nagpurnews
             </span>
           </a>
-          <div className="md:flex md:flex-row justify-start items-center hidden w-80 rounded-md p-1.5 bg-inherit border border-primary-600">
+          {/* <div className="md:flex md:flex-row justify-start items-center hidden w-80 rounded-md px-1.5 py-1 bg-inherit border border-primary-600">
             <input
               className="w-full bg-inherit focus:outline-none mr-2"
               placeholder="Search"
@@ -31,43 +38,89 @@ export default function Navbar() {
             <span className="text-xl text-primary-800 inline-flex items-center p-1 hover:bg-primary-100 rounded-full">
               <FiSearch />
             </span>
+          </div> */}
+          <div className="hidden md:flex justify-center items-center gap-2 mx-2">
+            <span className="text-sm font-bold uppercase text-primary-800">
+              Follow us on
+            </span>
+            <SocialLinkForPromotionaryArticle
+              icon={<BsWhatsapp />}
+              link="https://wa.me/message/LWMXJ6WS46UYO1"
+            />
+            <SocialLinkForPromotionaryArticle
+              icon={<BsFacebook />}
+              link="https://www.facebook.com/profile.php?id=100064034115032&mibextid=LQQJ4d"
+            />
+            <SocialLinkForPromotionaryArticle
+              icon={<BsInstagram />}
+              link="https://instagram.com/nagpurnews?igshid=OGQ5ZDc2ODk2ZA=="
+            />
+            <SocialLinkForPromotionaryArticle
+              icon={<BsTwitter />}
+              link="https://twitter.com/nagpurnews3?s=11&t=8HMRzhBp7cDyqAwaBU2sRA"
+            />
           </div>
           <button
             type="button"
             onClick={() => setOpen(!open)}
-            className="inline-flex items-center p-2 ml-3 text-2xl text-primary-50 bg-primary-800 rounded-lg sm:hidden focus:outline-none"
+            className="inline-flex items-center p-2 ml-3 text-2xl text-primary-50 bg-primary-900 rounded-lg sm:hidden focus:outline-none"
           >
             {open ? <FiChevronsLeft /> : <FiChevronsRight />}
           </button>
         </div>
       </nav>
-      <nav className="top-[73px] fixed w-full bg-primary-100 border-b border-primary-600">
-        <div className="max-w-screen-xl px-3 py-2 mx-auto">
-          <div className="flex items-center">
-            <ul className="font-medium mt-0 mr-2 space-x-8 text-sm border-r border-primary-600">
-              <NavPill href="/" label="Home" />
-            </ul>
-            <ul className="flex flex-row overflow-x-auto scrollbar-hide">
-              <NavPill href="/" label="Category" />
-              <NavPill href="/" label="Category" />
-              <NavPill href="/" label="Category" />
-              <NavPill href="/" label="Category" />
-              <NavPill href="/" label="Category" />
-              <NavPill href="/" label="Category" />
-              <NavPill href="/" label="Category" />
-              <NavPill href="/" label="Category" />
-              <NavPill href="/" label="Category" />
-            </ul>
-          </div>
-        </div>
-      </nav>
       <aside
         className={`fixed top-0 right-0 w-[300px] md:hidden h-screen transition-transform ${
           open ? "translate-x-full" : "translate-x-0"
-        } duration-300`}
+        } duration-300 z-40`}
       >
-        <div className="h-full px-3 py-4 overflow-y-auto bg-primary-50 border-l border-primary-600">
-          <ul className="space-y-2 font-medium"></ul>
+        <div className="h-full flex flex-col justify-between bg-primary-50 border-l pt-20 border-primary-600">
+          <ul className="space-y-2 overflow-y-auto font-medium">
+            <li className="px-4 py-1.5 flex items-center border-b border-primary-600">
+              <span className="mr-2">
+                <BiSolidCategory />
+              </span>
+              Categories
+            </li>
+            <ul className="space-y-2 overflow-y-auto font-medium flex flex-col items-start gap-1 scrollbar-hide px-3 my-2">
+              {categories.map((category) => (
+                <Link key={category.id} href={`/category/${category.id}`}>
+                  <li
+                    key={category.id}
+                    onClick={() => setOpen(!open)}
+                    className="cursor-pointer px-2.5 py-1.5 text-sm text-primary-800 rounded-md bg-primary-100 border border-primary-600"
+                  >
+                    {category.name}
+                  </li>
+                </Link>
+              ))}
+            </ul>
+          </ul>
+          <ul className="flex flex-col justify-center items-center gap-2 border-t border-primary-600 bg-primary-900 py-4">
+            <li className="flex gap-2 justify-center items-center">
+              <SocialLinkForPromotionaryArticle
+                icon={<BsWhatsapp />}
+                link="https://wa.me/message/LWMXJ6WS46UYO1"
+              />
+              <SocialLinkForPromotionaryArticle
+                icon={<BsFacebook />}
+                link="https://www.facebook.com/profile.php?id=100064034115032&mibextid=LQQJ4d"
+              />
+              <SocialLinkForPromotionaryArticle
+                icon={<BsInstagram />}
+                link="https://instagram.com/nagpurnews?igshid=OGQ5ZDc2ODk2ZA=="
+              />
+              <SocialLinkForPromotionaryArticle
+                icon={<BsTwitter />}
+                link="https://twitter.com/nagpurnews3?s=11&t=8HMRzhBp7cDyqAwaBU2sRA"
+              />
+            </li>
+            <li>
+              <span className="w-full flex justify-center md:justify-start text-[12px] text-primary-50">
+                2023 &copy; Copyright Nagpur News
+              </span>
+            </li>
+          </ul>
         </div>
       </aside>
     </>
