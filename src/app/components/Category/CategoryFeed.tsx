f990604a179bd95a12f05cf681309abe2604f12f
GulshanSS <sahugulshan686@gmail.com>
2023-07-30 20:18:54 +0530

    Add: CategoryFeed Component

--- src/app/components/Category/CategoryFeed.tsx
+++ src/app/components/Category/CategoryFeed.tsx
@@ -1,7 +1,28 @@
-import React from 'react'
+import React from "react";
+import { Category } from "../../../../types";
+import ArticleCard from "../shared/ArticleCard";
+import ArticleCardForCategory from "../shared/ArticleCardForCategory";
+import Link from "next/link";
 
-export default function CategoryFeed() {
-  return (
-    <div>CategoryFeed</div>
-  )
+type Props = {
+  category: Category;
+};
+
+export default function CategoryFeed({ category }: Props) {
+  const content = (
+    <div className="mx-2 p-2 rounded-md mb-6">
+      <div className="mb-6 font-semibold text-2xl text-primary-800 uppercase">
+        {category.name}
+      </div>
+      <div className="flex flex-wrap md:flex-nowrap justify-center md:justify-start gap-2">
+        {category.article.map((article) => (
+          <Link href={`/article/${article.id}`}>
+            <ArticleCardForCategory key={article.id} article={article} />
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
+
+  return content;
 }
