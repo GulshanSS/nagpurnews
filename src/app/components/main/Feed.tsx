41913d680904ac8eaecde2c1a8ea448646b26775
GulshanSS <sahugulshan686@gmail.com>
2023-07-30 20:19:48 +0530

    Add: Feed Component

--- src/app/components/main/Feed.tsx
+++ src/app/components/main/Feed.tsx
@@ -1,5 +1,19 @@
 import React from "react";
+import { Article } from "../../../../types";
+import ArticleCard from "../shared/ArticleCard";
 
-export default function Feed() {
-  return <div>Mail Feed</div>;
+type Props = {
+  articles: Article[];
+};
+
+export default function Feed({ articles }: Props) {
+  return (
+    <div className="mx-2 p-2">
+      <div className="my-4 flex flex-col gap-2">
+        {articles.map((article) => (
+          <ArticleCard key={article.id} article={article} />
+        ))}
+      </div>
+    </div>
+  );
 }
