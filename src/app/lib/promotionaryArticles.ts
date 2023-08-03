55a077b9d566fbfa40d5cb42a9b236558f0360a6
GulshanSS <sahugulshan686@gmail.com>
2023-08-03 15:19:51 +0530

    Add: Revalidate

--- src/app/lib/promotionaryArticles.ts
+++ src/app/lib/promotionaryArticles.ts
@@ -1,6 +1,8 @@
 const BASE_URL = `${process.env.NEXT_PUBLIC_BACKEND_URI}/api/v1/public`;
 
 export async function getAllPromotionaryArticlesAsBanners() {
-  const res = await fetch(`${BASE_URL}/promotionary-article/banner/all`);
+  const res = await fetch(`${BASE_URL}/promotionary-article/banner/all`, {
+    next: { revalidate: 60 },
+  });
   return res.json();
 }
