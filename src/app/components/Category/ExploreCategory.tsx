c9af58c93f174dbc717e47ba98a103fb591e91d3
GulshanSS <sahugulshan686@gmail.com>
2023-08-06 02:32:47 +0530

    Add: Margin

--- src/app/components/Category/ExploreCategory.tsx
+++ src/app/components/Category/ExploreCategory.tsx
@@ -8,15 +8,15 @@ type Props = {
 
 export default function ExploreCategory({ categories }: Props) {
   return (
-    <div className="mx-2 p-4 flex flex-col rounded-md bg-primary-900">
-      <span className="mb-6 font-semibold text-2xl text-primary-100 uppercase">
+    <div className="m-4 p-4 flex flex-col rounded-md bg-primary-900">
+      <span className="mb-6 font-semibold text-2xl text-primary-50 uppercase">
         Explore Other Categories
       </span>
-      <div className="flex flex-wrap gap-2 text-primary-100">
+      <div className="flex flex-wrap gap-2 text-primary-50">
         {categories.map((category) => (
           <Link key={category.id} href={`/category/${category.id}`}>
             <div
-              className="uppercase text-[12px] px-2.5 hover:bg-primary-100 hover:text-primary-800 border-2 border-primary-100 rounded-md cursor-pointer"
+              className="uppercase text-[12px] font-thin px-2.5 hover:bg-primary-50 hover:text-primary-900 border-2 border-primary-50 rounded-md cursor-pointer"
             >
               {category.name}
             </div>
