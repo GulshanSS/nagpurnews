88144690eb69bd8675f8b63bbfd8337ae07d7e5f
GulshanSS <sahugulshan686@gmail.com>
2023-08-06 01:43:48 +0530

    Fix: Text Color

--- src/app/components/Navigation/NavPill.tsx
+++ src/app/components/Navigation/NavPill.tsx
@@ -12,7 +12,7 @@ type Props = {
 export default function NavPill({ href, label, icon }: Props) {
   return (
     <Link href={href}>
-      <li className="hover:bg-primary-100 hover:text-primary-800 text-primary-100 text-sm text whitespace-nowrap font-medium px-2 py-1 rounded-sm duration-300 mr-2">
+      <li className="hover:bg-primary-50 hover:text-primary-800 text-primary-50 text-sm text whitespace-nowrap font-medium px-2 py-1 rounded-sm duration-300 mr-2">
         {icon ? <span className="text-xl">{icon}</span> : label}
       </li>
     </Link>
