5cc5f33471e32d183c2f779bac6bb245b492cffa
GulshanSS <sahugulshan686@gmail.com>
2023-08-06 02:32:26 +0530

    Fix: Render only when articles are there

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -44,7 +44,7 @@ export default async function RootLayout({
         <main className="max-w-screen-xl min-h-screen flex flex-wrap mx-auto text-3xl my-10 font-bold">
           <div className="md:w-2/3">{children}</div>
           <div className="md:w-1/3">
-            <LatestFeed articles={latestArticles} />
+            {latestArticles && <LatestFeed articles={latestArticles} />}
             <PromotionaryArticleFeed
               promotionaryArticles={promotionaryArticlesAsBanner}
             />
