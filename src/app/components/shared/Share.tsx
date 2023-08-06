35804f4ff9233e7239c0777ab1d7bb5e047e1ae7
GulshanSS <sahugulshan686@gmail.com>
2023-08-07 03:32:25 +0530

    Add: Share Component

---
+++ src/app/components/shared/Share.tsx
@@ -0,0 +1,89 @@
+"use client";
+import React, { useEffect, useState } from "react";
+import {
+  BsWhatsapp,
+  BsFacebook,
+  BsTelegram,
+  BsTwitter,
+  BsShareFill,
+} from "react-icons/bs";
+
+type Props = {
+  url: string;
+  title: string;
+};
+
+export default function Share({ url, title }: Props) {
+  const BaseUrl = process.env.NEXT_PUBLIC_CLIENT_URI as string;
+  const shareData = {
+    title,
+    url: `${BaseUrl}/${url}`,
+  };
+  const [showNativeShare, setShowNativeShare] = useState<boolean>(false);
+  useEffect(() => {
+    if (!navigator.canShare) {
+      setShowNativeShare(false);
+    } else if (navigator.canShare(shareData)) {
+      setShowNativeShare(true);
+    }
+  }, []);
+
+  return (
+    <>
+      <ul className="flex gap-8 justify-between md:justify-start items-center text-gray-500 mb-4 mx-2.5">
+        <li>
+          <a
+            target="_blank"
+            href={`whatsapp://send?text=${title} ${BaseUrl}/${url}`}
+          >
+            <span className="text-xl rounded-full">
+              <BsWhatsapp />
+            </span>
+          </a>
+        </li>
+        <li>
+          <a
+            target="_blank"
+            href={`https://www.facebook.com/sharer/sharer.php?u=${BaseUrl}/${url}`}
+          >
+            <span className="text-xl rounded-full">
+              <BsFacebook />
+            </span>
+          </a>
+        </li>
+        <li>
+          <a
+            target="_blank"
+            href={`tg://msg_url?url=${BaseUrl}/${url}&text=${title}`}
+          >
+            <span className="text-xl rounded-full">
+              <BsTelegram />
+            </span>
+          </a>
+        </li>
+        <li>
+          <a
+            target="_blank"
+            href={`http://twitter.com/share?text=${title}&url=${BaseUrl}/${url}`}
+          >
+            <span className="text-xl rounded-full">
+              <BsTwitter />
+            </span>
+          </a>
+        </li>
+        {showNativeShare && (
+          <li>
+            <a
+              target="_blank"
+              href={`http://twitter.com/share?text=${title}&url=${BaseUrl}/${url}`}
+            >
+              <span className="text-xl rounded-full">
+                <BsShareFill />
+              </span>
+            </a>
+          </li>
+        )}
+      </ul>
+    </>
+  );
+}
