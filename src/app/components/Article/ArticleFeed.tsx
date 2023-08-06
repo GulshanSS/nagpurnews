20a46c5f4c93eb40e0f624c791ccc7bcb57ae118
GulshanSS <sahugulshan686@gmail.com>
2023-08-07 03:30:37 +0530

    Add: Share options to article feed

--- src/app/components/Article/ArticleFeed.tsx
+++ src/app/components/Article/ArticleFeed.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { Article } from "../../../../types";
 import Carousel from "../shared/Carousel";
+import Share from "../shared/Share";
+import { BsShare, BsShareFill } from "react-icons/bs";
 
 type Props = {
   article: Article;
@@ -10,11 +12,11 @@ export default function ArticleFeed({ article }: Props) {
   return (
     <div className="mx-2 p-2 rounded-md mb-6">
       <Carousel slides={article.media} />
-      <div className="font-semibold text-xl md:text-2xl text-primary-800">
+      <div className="font-semibold text-justify text-xl md:text-2xl text-primary-800">
         {article.title}
       </div>
-      <div className="text-primary-800 font-semibold uppercase">
-        <div className="text-[10px] font-semibold">
+      <div className="text-gray-500 font-semibold uppercase">
+        <div className="text-[12px]">
           {article.location},{" "}
           {new Intl.DateTimeFormat("indian", {
             year: "numeric",
@@ -27,9 +29,16 @@ export default function ArticleFeed({ article }: Props) {
         </div>
       </div>
       <div
-        className="text-[16px] font-light text-primary-800 leading-6 text-justify"
+        className="text-[16px] font-light text-primary-800 leading-6 text-justify mb-6"
         dangerouslySetInnerHTML={{ __html: article.content }}
       ></div>
+      <div className="w-full flex justify-center md:justify-start gap-2 items-center text-center text-red-600 text-sm md:mx-2 mb-4 uppercase">
+        <span>
+          <BsShare />
+        </span>
+        Share
+      </div>
+      <Share url={`article/${article.id}`} title={article.title} />
     </div>
   );
 }
