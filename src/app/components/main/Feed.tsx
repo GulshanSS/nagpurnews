509c4fe6fd06448e669dfcfce593a37c6e905f32
GulshanSS <sahugulshan686@gmail.com>
2023-08-07 19:01:12 +0530

    Add: Scroll button

--- src/app/components/main/Feed.tsx
+++ src/app/components/main/Feed.tsx
@@ -1,6 +1,8 @@
+"use client";
 import React from "react";
 import { Article } from "../../../../types";
 import ArticleCard from "../shared/ArticleCard";
+import ScrollButton from "../shared/ScrollButton";
 
 type Props = {
   articles: Article[];
@@ -10,10 +12,12 @@ export default function Feed({ articles }: Props) {
   return (
     <div className="mx-2 p-2">
       <div className="my-4 flex flex-col gap-2">
-        {articles && articles.map((article) => (
-          <ArticleCard key={article.id} article={article} />
-        ))}
+        {articles &&
+          articles.map((article) => (
+            <ArticleCard key={article.id} article={article} />
+          ))}
       </div>
+      <ScrollButton />
     </div>
   );
 }
