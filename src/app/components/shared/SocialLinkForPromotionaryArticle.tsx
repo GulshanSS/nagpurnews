4dca86cebff143028085301a49650a52be55953d
GulshanSS <sahugulshan686@gmail.com>
2023-08-28 11:42:10 +0530

    Add: Colors to Social Icons

--- src/app/components/shared/SocialLinkForPromotionaryArticle.tsx
+++ src/app/components/shared/SocialLinkForPromotionaryArticle.tsx
@@ -3,16 +3,26 @@ import React from "react";
 type Props = {
   icon: JSX.Element;
   link: string;
+  socialMediaName: string;
 };
 
 export default function SocialLinkForPromotionaryArticle({
   icon,
   link,
+  socialMediaName,
 }: Props) {
+  const colorVariant: { [key: string]: string } = {
+    youtube: "text-red-600",
+    whatsapp: "text-green-600",
+    twitter: "text-blue-400",
+    facebook: "text-blue-800",
+    instagram: "text-pink-800",
+  };
+
   return (
     <a
       target="_blank"
-      className="rounded-full w-10 h-10 flex justify-center items-center text-xl border border-primary-600 bg-primary-100 text-primary-800 hover:bg-primary-800 hover:text-primary-100"
+      className={`rounded-full w-10 h-10 flex justify-center items-center text-xl ${colorVariant[socialMediaName]} hover:bg-primary-900 hover:text-primary-50`}
       href={link}
     >
       {icon}
