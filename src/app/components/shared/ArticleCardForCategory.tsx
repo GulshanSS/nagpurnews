0aa61553f21781debe203c260b102ba465784423
GulshanSS <sahugulshan686@gmail.com>
2023-09-04 07:38:46 +0530

    Fix: UI changes

--- src/app/components/shared/ArticleCardForCategory.tsx
+++ src/app/components/shared/ArticleCardForCategory.tsx
@@ -7,9 +7,9 @@ type Props = {
 
 export default function ArticleCardForCategory({ article }: Props) {
   return (
-    <div className="w-80 md:w-60 lg:w-64 flex flex-col justify-center gap-2 rounded-md cursor-pointer">
+    <div className="relative overflow-hidden md:w-96 flex flex-col justify-center gap-2 rounded-md cursor-pointer">
       {article.media[0].type.startsWith("image/") && (
-        <div className="w-full h-52">
+        <div className="w-full h-64">
           <img
             loading="lazy"
             className="w-full h-full overflow-hidden text-sm object-cover rounded-md"
@@ -20,13 +20,13 @@ export default function ArticleCardForCategory({ article }: Props) {
       )}
       {article.media[0].type.startsWith("video/") && (
         <video
-          className="flex justify-center w-full h-52 bg-black rounded-md"
+          className="flex justify-center bg-black w-full md:h-64 rounded-md"
           controls={true}
           autoPlay={false}
           src={article.media[0].key}
         />
       )}
-      <span className="text-sm font-bold text-primary-800">
+      <span className="absolute p-2 flex w-full bg-black/30 backdrop-blur-lg bottom-0 text-sm font-bold text-white">
         {article.title}
       </span>
     </div>
