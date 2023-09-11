afa6f0531721940b8502541ae4490661d00faadf
GulshanSS <sahugulshan686@gmail.com>
2023-09-11 22:34:10 +0530

    Fix: SkeletonCard

--- src/app/components/shared/SkeletonCard.tsx
+++ src/app/components/shared/SkeletonCard.tsx
@@ -1,7 +1,7 @@
 export default function SkeletonCard() {
   return (
     <>
-      <div className="min-w-80 mx-2 my-4 flex gap-2 h-32 rounded-md p-2.5 border border-gray-200 shadow-md select-none">
+      <div className="min-w-80 mx-2 my-4 flex gap-2 h-32 rounded-md p-2 border border-gray-200 shadow-md select-none">
         <div className="w-full">
           <div className="h-2.5 bg-gray-200 animate-pulse rounded-full w-48 mb-4"></div>
           <div className="h-2 bg-gray-200 animate-pulse rounded-full max-w-[480px] mb-2.5"></div>
@@ -10,7 +10,7 @@ export default function SkeletonCard() {
           <div className="h-2 bg-gray-200 animate-pulse rounded-full max-w-[460px] mb-2.5"></div>
           <div className="h-2 bg-gray-200 animate-pulse rounded-full max-w-[360px]"></div>
         </div>
-        <div className="flex items-center justify-center w-40 bg-gray-300 animate-pulse rounded"></div>
+        <div className="flex items-center justify-center w-20 bg-gray-300 animate-pulse rounded"></div>
         <span className="sr-only">Loading...</span>
       </div>
     </>
