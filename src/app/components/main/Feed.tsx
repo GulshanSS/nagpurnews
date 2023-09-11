2668e55a61d39e4500d089290c2279f89c2e6fab
GulshanSS <sahugulshan686@gmail.com>
2023-09-11 20:17:44 +0530

    Add: Paginated results for main feed articles

--- src/app/components/main/Feed.tsx
+++ src/app/components/main/Feed.tsx
@@ -1,14 +1,32 @@
 "use client";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Article } from "../../../../types";
 import ArticleCard from "../shared/ArticleCard";
 import ScrollButton from "../shared/ScrollButton";
+import { getMainFeedArticles } from "@/app/lib/article";
+import Pagination from "../shared/Pagination";
+import SkeletonCard from "../shared/SkeletonCard";
 
-type Props = {
-  articles: Article[];
-};
+export default function Feed() {
+  const [page, setPage] = useState<number>(1);
+  const [pages, setPages] = useState<number>(1);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [articles, setArticles] = useState<Article[]>([]);
 
-export default function Feed({ articles }: Props) {
+  useEffect(() => {
+    (async () => {
+      setLoading(true);
+      const data = await getMainFeedArticles(page);
+      setArticles(data.articles);
+      setPages(data.pages);
+      setLoading(false);
+    })();
+  }, [page]);
+
+  if (loading) {
+    return <SkeletonCard />;
+  }
+
   return (
     <div className="mx-2 p-2">
       <div className="my-4 flex flex-col gap-2">
@@ -16,6 +34,9 @@ export default function Feed({ articles }: Props) {
           articles.map((article) => (
             <ArticleCard key={article.id} article={article} />
           ))}
+      </div>
+      <div className="w-80 mx-auto py-4">
+        <Pagination page={page} pages={pages} changePage={setPage} />
       </div>
       <ScrollButton />
     </div>
