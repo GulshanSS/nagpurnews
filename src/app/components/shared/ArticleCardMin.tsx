47004470d8808c6065b6a6fd102f4173e7de4b87
GulshanSS <sahugulshan686@gmail.com>
2023-09-12 20:07:42 +0530

    Add: Article Card Min Component

--- src/app/components/shared/ArticleCardMin.tsx
+++ src/app/components/shared/ArticleCardMin.tsx
@@ -1,5 +1,36 @@
-import React from "react";
+import { Article } from "../../../../types";
+import Link from "next/link";
 
-export default function ArticleCardMin() {
-  return <div>ArticleCardMin</div>;
+type Props = {
+  article: Article;
+};
+
+export default function ArticleCardMin({ article }: Props) {
+  return (
+    <Link href={`/article/${article.slug}`}>
+      <div className="relative w-80 flex flex-col justify-center gap-2 rounded-md cursor-pointer">
+        {article.media[0].type.startsWith("image/") && (
+          <div className="w-full h-52">
+            <img
+              loading="lazy"
+              className="w-full h-full overflow-hidden text-sm object-cover rounded-md"
+              alt={article.title}
+              src={article.media[0].key}
+            />
+          </div>
+        )}
+        {article.media[0].type.startsWith("video/") && (
+          <video
+            className="flex justify-center bg-black w-full h-52 rounded-md"
+            controls={true}
+            autoPlay={false}
+            src={article.media[0].key}
+          />
+        )}
+        <span className="absolute p-2 flex w-full bg-black/30 backdrop-blur-lg bottom-0 text-sm font-bold text-white">
+          {article.title}
+        </span>
+      </div>
+    </Link>
+  );
 }
