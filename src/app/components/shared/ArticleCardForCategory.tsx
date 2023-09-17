eecec50444855953a4808b782c1d29f1f3ae7fef
GulshanSS <sahugulshan686@gmail.com>
2023-09-17 23:56:09 +0530

    Fix: Media Dimensions

--- src/app/components/shared/ArticleCardForCategory.tsx
+++ src/app/components/shared/ArticleCardForCategory.tsx
@@ -7,20 +7,18 @@ type Props = {
 
 export default function ArticleCardForCategory({ article }: Props) {
   return (
-    <div className="relative overflow-hidden md:w-96 flex flex-col justify-center gap-2 rounded-md cursor-pointer">
+    <div className="relative overflow-hidden w-[400px] md:w-96 flex flex-col justify-center gap-2 rounded-md cursor-pointer bg-black">
       {article.media[0].type.startsWith("image/") && (
-        <div className="w-full h-64">
-          <img
-            loading="lazy"
-            className="w-full h-full overflow-hidden text-sm object-cover rounded-md"
-            alt={article.title}
-            src={article.media[0].key}
-          />
-        </div>
+        <img
+          loading="lazy"
+          className="w-full h-64 overflow-hidden text-sm object-contain rounded-md"
+          alt={article.title}
+          src={article.media[0].key}
+        />
       )}
       {article.media[0].type.startsWith("video/") && (
         <video
-          className="flex justify-center bg-black w-full md:h-64 rounded-md"
+          className="flex justify-center bg-black w-full h-64 rounded-md"
           controls={true}
           autoPlay={false}
           src={article.media[0].key}
