5256d973d1284c7fe93087d6eafe22c0139adc3c
GulshanSS <sahugulshan686@gmail.com>
2023-09-29 18:46:02 +0530

    Fix: Rendering Images using Dimensions

--- src/app/components/shared/ArticleCardForCategory.tsx
+++ src/app/components/shared/ArticleCardForCategory.tsx
@@ -6,12 +6,25 @@ type Props = {
 };
 
 export default function ArticleCardForCategory({ article }: Props) {
+  const checkDimension = (imgUrl: string) => {
+    const img = new Image();
+    img.src = imgUrl;
+    if (img.height < img.width) {
+      return true;
+    }
+    return false;
+  };
+
   return (
     <div className="relative overflow-hidden md:w-96 flex flex-col justify-center gap-2 rounded-md cursor-pointer bg-black">
       {article.media[0].type.startsWith("image/") && (
         <img
           loading="lazy"
-          className="w-full h-64 overflow-hidden text-sm object-contain rounded-md"
+          className={`w-full h-64 overflow-hidden text-sm ${
+            checkDimension(article.media[0].key)
+              ? "object-cover"
+              : "object-contain"
+          } rounded-md`}
           alt={article.title}
           src={article.media[0].key}
         />
