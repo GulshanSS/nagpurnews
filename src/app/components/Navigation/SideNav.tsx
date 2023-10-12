fba191c913f6fbac7c454cbde84648248acb6a57
GulshanSS <sahugulshan686@gmail.com>
2023-10-12 08:05:46 +0530

    Fix: UI changes

--- src/app/components/Navigation/SideNav.tsx
+++ src/app/components/Navigation/SideNav.tsx
@@ -1,16 +1,17 @@
-import SocialLinkForPromotionaryArticle from "../shared/SocialLinkForPromotionaryArticle";
-import { FiChevronsRight } from "react-icons/fi";
 import { BiSolidCategory } from "react-icons/bi";
+import { BsPeopleFill } from "react-icons/bs";
+import { Category } from "../../../../types";
+import Link from "next/link";
+import Image from "next/image";
+import logo from "@/app/assets/logo.jpg";
+import SocialLinkForPromotionaryArticle from "../shared/SocialLinkForPromotionaryArticle";
 import {
-  BsPeopleFill,
   BsFacebook,
   BsInstagram,
   BsTwitter,
   BsWhatsapp,
   BsYoutube,
 } from "react-icons/bs";
-import { Category } from "../../../../types";
-import Link from "next/link";
 
 type Props = {
   categories: Category[];
@@ -22,25 +23,31 @@ export default function SideNav({ open, setOpen, categories }: Props) {
   return (
     <>
       <aside
-        className={`fixed top-0 right-0 w-[300px] md:hidden h-screen transition-transform ${
+        className={`fixed top-0 right-0 md:hidden w-screen h-screen transition-transform duration-300 z-40 ${
           open ? "translate-x-full" : "translate-x-0"
-        } duration-300 z-40`}
+        }`}
       >
         <div className="h-full flex flex-col justify-between bg-white border-l border-primary-600">
-          <ul className="space-y-2 overflow-y-auto font-medium">
-            <span className="w-full flex justify-end items-center pr-5 pt-5">
-              <button
-                type="button"
-                onClick={() => setOpen(true)}
-                className="p-2 ml-3 text-2xl text-primary-50 bg-primary-900 rounded-lg sm:hidden focus:outline-none"
+          <ul className="space-y-2 overflow-y-auto font-medium pb-[100px]">
+            <Link href="/">
+              <li
+                onClick={() => setOpen(!open)}
+                className="w-full flex justify-start items-center px-4 py-3.5 bg-primary-900 border-b border-primary-600"
               >
-                <FiChevronsRight />
-              </button>
-            </span>
+                <Image
+                  src={logo}
+                  className="w-6 md:w-8 mr-3 rounded-full ring-2 ring-primary-600"
+                  alt="Nagpur News Logo"
+                />
+                <span className="self-center text-xl md:text-2xl text-primary-100 font-bold whitespace-nowrap uppercase">
+                  Nagpur News
+                </span>
+              </li>
+            </Link>
             <Link href="/category">
               <li
                 onClick={() => setOpen(!open)}
-                className="px-4 py-1.5 flex items-center border-b border-primary-600"
+                className="px-4 py-2.5 flex items-center"
               >
                 <span className="mr-2">
                   <BiSolidCategory />
@@ -48,7 +55,7 @@ export default function SideNav({ open, setOpen, categories }: Props) {
                 Categories
               </li>
             </Link>
-            <ul className="space-y-2 overflow-y-auto font-medium flex flex-col items-start gap-1 scrollbar-hide px-3 my-2">
+            <ul className="overflow-y-auto font-medium flex flex-wrap items-start gap-2 scrollbar-hide px-3 my-2">
               {categories &&
                 categories.map((category) => (
                   <Link key={category.id} href={`/category/${category.slug}`}>
@@ -66,7 +73,7 @@ export default function SideNav({ open, setOpen, categories }: Props) {
               <Link href="/aboutus">
                 <li
                   onClick={() => setOpen(true)}
-                  className="px-4 py-1.5 flex items-center border-y border-primary-600"
+                  className="px-4 py-2.5 flex items-center"
                 >
                   <span className="mr-2">
                     <BsPeopleFill />
@@ -75,40 +82,38 @@ export default function SideNav({ open, setOpen, categories }: Props) {
                 </li>
               </Link>
             </ul>
-          </ul>
-          <ul className="flex flex-col justify-center items-center gap-2 border-t border-primary-600 bg-primary-900 py-4">
-            <li className="flex gap-2 justify-center items-center">
-              <SocialLinkForPromotionaryArticle
-                icon={<BsYoutube />}
-                link="https://youtube.com/@nagpurnews1"
-                socialMediaName="youtube"
-              />
-              <SocialLinkForPromotionaryArticle
-                icon={<BsFacebook />}
-                link="https://www.facebook.com/profile.php?id=100064034115032&mibextid=LQQJ4d"
-                socialMediaName="facebook"
-              />
-              <SocialLinkForPromotionaryArticle
-                icon={<BsInstagram />}
-                link="https://instagram.com/nagpurnews?igshid=OGQ5ZDc2ODk2ZA=="
-                socialMediaName="instagram"
-              />
-              <SocialLinkForPromotionaryArticle
-                icon={<BsTwitter />}
-                link="https://twitter.com/nagpurnews3?s=11&t=8HMRzhBp7cDyqAwaBU2sRA"
-                socialMediaName="twitter"
-              />
-              <SocialLinkForPromotionaryArticle
-                icon={<BsWhatsapp />}
-                link="https://wa.me/message/LWMXJ6WS46UYO1"
-                socialMediaName="whatsapp"
-              />
-            </li>
-            <li>
-              <span className="w-full flex justify-center md:justify-start text-[12px] text-primary-50">
-                2023 &copy; Copyright Nagpur News
+            <ul className="flex flex-col justify-center items-center gap-2 px-4 py-8">
+              <span className="flex text-sm font-bold uppercase text-slate-400 tracking-widest">
+                Follow us on
               </span>
-            </li>
+              <ul className="flex gap-2">
+                <SocialLinkForPromotionaryArticle
+                  icon={<BsYoutube />}
+                  link="https://youtube.com/@nagpurnews1"
+                  socialMediaName="youtube"
+                />
+                <SocialLinkForPromotionaryArticle
+                  icon={<BsFacebook />}
+                  link="https://www.facebook.com/profile.php?id=100064034115032&mibextid=LQQJ4d"
+                  socialMediaName="facebook"
+                />
+                <SocialLinkForPromotionaryArticle
+                  icon={<BsInstagram />}
+                  link="https://instagram.com/nagpurnews?igshid=OGQ5ZDc2ODk2ZA=="
+                  socialMediaName="instagram"
+                />
+                <SocialLinkForPromotionaryArticle
+                  icon={<BsTwitter />}
+                  link="https://twitter.com/nagpurnews3?s=11&t=8HMRzhBp7cDyqAwaBU2sRA"
+                  socialMediaName="twitter"
+                />
+                <SocialLinkForPromotionaryArticle
+                  icon={<BsWhatsapp />}
+                  link="https://wa.me/message/LWMXJ6WS46UYO1"
+                  socialMediaName="whatsapp"
+                />
+              </ul>
+            </ul>
           </ul>
         </div>
       </aside>
