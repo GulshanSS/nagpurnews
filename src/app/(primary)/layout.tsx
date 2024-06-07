956635c5a6571e5ff8c205a6077a1dad97cc3d00
GulshanSS <sahugulshan686@gmail.com>
2024-06-07 23:51:52 +0530

    Added: Google ads link

--- src/app/(primary)/layout.tsx
+++ src/app/(primary)/layout.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Script from "next/script";
 import LatestFeed from "../components/Latest/LatestFeed";
 import PromotionaryArticleFeed from "../components/PromotionaryArticle/PromotionaryArticleFeed";
 import { getLatestArticles } from "../lib/article";
@@ -19,6 +20,12 @@ export default async function MainLayout({
 
   return (
     <>
+      <Script
+        id="adsbygoogle-init"
+        strategy="beforeInteractive"
+        crossOrigin="anonymous"
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5092077595720219"
+      />
       <div className="w-full md:w-2/3">{children}</div>
       <div className="w-full md:w-1/3">
         {latestArticles && <LatestFeed articles={latestArticles} />}
