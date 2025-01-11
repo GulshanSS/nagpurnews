09a8d5ec5e8125666f60e59edda45f36e68bc59f
GulshanSS <sahugulshan686@gmail.com>
2025-01-11 17:09:19 +0530

    Fixed: Image Overflow from Article Card

--- src/app/components/shared/ArticleCard.tsx
+++ src/app/components/shared/ArticleCard.tsx
@@ -10,7 +10,7 @@ type Props = {
 
 export default function ArticleCard({ article }: Props) {
   return (
-    <div className="min-w-80 flex flex-col gap-2 rounded-md px-2.5 border border-gray-200 shadow-md cursor-pointer">
+    <div className="min-w-80 flex flex-col gap-2 rounded-md px-2.5 border border-gray-200 shadow-md cursor-pointer overflow-hidden">
       <div className="flex items-center gap-2">
         {article.category &&
           article.category.map((category) => (
@@ -29,13 +29,13 @@ export default function ArticleCard({ article }: Props) {
           ))}
       </div>
       <Link href={`/article/${article.slug}`}>
-        <div className="flex gap-2 mb-2">
-          <div>
+        <div className="flex flex-col sm:flex-row gap-2 mb-2 flex-wrap">
+          <div className="flex-1">
             <p className="text-sm md:text-xl font-bold text-primary-800 line-clamp-div article-card">
               {article.title}
             </p>
             <div
-              className="text-[14px] w-full leading-6 text-justify font-thin text-gray-500 line-clamp-div article-card"
+              className="text-[14px] leading-6 text-justify font-thin text-gray-500 line-clamp-div article-card"
               dangerouslySetInnerHTML={{ __html: article.content }}
             ></div>
           </div>
@@ -43,7 +43,7 @@ export default function ArticleCard({ article }: Props) {
             article.media[0].type.startsWith("image/") && (
               <img
                 loading="lazy"
-                className="flex-shrink-0 w-[100px] md:w-[150px] h-24 object-contain rounded-md max-w-full"
+                className="w-full sm:w-[150px] h-auto object-cover rounded-md max-w-full max-h-full"
                 alt={article.title}
                 src={article.media[0].key}
               />
@@ -52,7 +52,7 @@ export default function ArticleCard({ article }: Props) {
             article.media[0].type.startsWith("video/") && (
               <video
                 playsInline
-                className="flex-shrink-0 min-w-[100px] md:min-w-[150px] h-24 bg-black rounded-md max-w-full"
+                className="w-full sm:w-[150px] h-auto bg-black rounded-md max-w-full"
                 controls
                 loop
                 autoPlay
